2a7ed3e89da4e4f5c8b77dac37bb386501a70e82
Ganesh-Mariappan <ganesh.mariappan@syncfusion.com>
2024-02-14 18:59:59 +0530

    Added the CardView customization sample.

---
+++ ItemTemplate_Customization/CardView_142838/Model/Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardView_142838
+{
+    public class Model
+    {
+        //Properties
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+       
+        private string header;
+        public string Header
+        {
+            get { return header; }
+            set { header = value; }
+        }
+
+    }
+}
---
+++ ItemTemplate_Customization/CardView_142838/ViewModel/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace CardView_142838
+{
+
+    public class ViewModel
+    {       
+        private ObservableCollection<Model> collection;
+        public ObservableCollection<Model> CardViewcollections
+        {
+            get { return collection; }
+            set { collection = value; }
+        }
+   
+        public ViewModel()
+        {
+            CardViewcollections = new ObservableCollection<Model>();
+            GenerateItems();       
+        }        
+        private void GenerateItems()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                collection.Add(new Model() { Name = "Item" + i, Header = "Header" + i });
+            }
+        }
+    }
+}
